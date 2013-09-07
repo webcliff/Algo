33f240a541ed6d02e450526a05a4fc1118540afe
webcliff <webcliff@gmail.com>
2013-09-07 00:06:13 -0700

    Update PriorityQueue.cs

--- PriorityQueue.cs
+++ PriorityQueue.cs
@@ -1,13 +1,13 @@
 using System;
 using System.Collections.Generic;
 
-// A Minimal Queue
+// MinQueue
 public class PriorityQueue<T> where T : IComparable<T>, new() {
 	
 	private List<T> mHeap;
 
-	public PriorityQueue() {
-		mHeap = new List<T>();
+	public PriorityQueue(int capacity) {
+		mHeap = new List<T>(capacity+1);
 		mHeap.Add(new T());
 	}
 
@@ -19,11 +19,13 @@ public class PriorityQueue<T> where T : IComparable<T>, new() {
 		return mHeap.Count-1;
 	}
 
+	// O(1)
 	public T Top() {
 		if(Empty()) throw new InvalidOperationException("empty queue");
 		return mHeap[1];
 	}
 
+	// O(logN)
 	public void Pop() {
 		if(Empty()) return;
 		swap(mHeap,1,mHeap.Count-1);
@@ -31,6 +33,7 @@ public class PriorityQueue<T> where T : IComparable<T>, new() {
 		MoveDown(1);
 	}
 
+	// O(logN)
 	public void Push(T t) {
 		mHeap.Add(t);
 		MoveUp(mHeap.Count-1);
@@ -57,34 +60,12 @@ public class PriorityQueue<T> where T : IComparable<T>, new() {
 		if(rc<mHeap.Count && mHeap[lc].CompareTo(mHeap[rc])>0) lc=rc;
 
 		if(mHeap[i].CompareTo(mHeap[lc])>0) swap(mHeap,i,lc);
-		
-		// Recusive for clean code
+
+		// recursively for clean code
 		MoveDown(lc);
 	}
 
 	private static void swap(List<T> heap, int a, int b) {
 		T t=heap[a]; heap[a]=heap[b]; heap[b]=t;
-	}
-}
-
-public class Test {
-		public static void Main() {
-		PriorityQueue<int> pq = new PriorityQueue<int>();
-		pq.Push(13);
-		pq.Push(8);
-		pq.Push(13);
-		pq.Push(19);
-		pq.Push(10);
-
-		Console.WriteLine(pq.Top());
-		pq.Pop();
-		Console.WriteLine(pq.Top());
-		pq.Pop();
-		Console.WriteLine(pq.Top());
-		pq.Pop();
-		Console.WriteLine(pq.Top());
-		pq.Pop();
-		Console.WriteLine(pq.Top());
-		pq.Pop();
 	}
 }
