5fd62320408a9c71f329320645887b7aebf71470
webcliff <webcliff@gmail.com>
2013-09-12 00:13:32 -0700

    Update Leetcode.BalancedBinaryTree.cs

--- Leetcode.BalancedBinaryTree.cs
+++ Leetcode.BalancedBinaryTree.cs
@@ -5,11 +5,11 @@
 
 	// For this problem, a height-balanced binary tree is defined as a binary tree 
 	// in which the depth of the two subtrees of every node never differ by more than 1.
-	
+
 	// CareerCup 4.1
-  ///////////////////////////
-  // Solution 1
-  //
+	//////////////////////////
+	// Solution 1
+	//
 	public bool IsBalancedBinaryTree(Node node, out int height) {
 		height = 0;
 		if(node==null) return true;
@@ -19,25 +19,25 @@
 		if(!IsBalancedBinaryTree(node.Right, out rH)) return false;
 
 		height=Math.Max(lH,rH)+1;
-		
+
 		return Math.Abs(lH-rH)<2;
 	}
 
-  // Solution 2
-  //
+	// Solution 2
+	//
 	public bool IsBalancedTree(Node node) {
 		if(node==null) return true;
 
 		int leftMaxDepth = GetMaxDepth(node.Left);
 		int rightMaxDepth = GetMaxDepth(node.Right);
-		
+
 		if(Math.Abs(rightMaxDepth-leftMaxDepth)>1) return false;
 
 		return IsBalancedTree(node.Left) && IsBalancedTree(node.Right);
 	}
 
-  // Solution 3
-  //
+	// Solution 3
+	//
 	public bool IsBalancedTreeIteratively(Node node) {
 		int min=int.MaxValue; int max=int.MinValue;
 		int h = 0;
@@ -60,15 +60,3 @@
 		return max-min>1;
 	}
 
-	public bool IsBalancedBinaryTree(Node node, out int height) {
-		height = 0;
-		if(node==null) return true;
-
-		int lH,rH;
-		if(!IsBalancedBinaryTree(node.Left,out lH)) return false;
-		if(!IsBalancedBinaryTree(node.Right, out rH)) return false;
-
-		height=Math.Max(lH,rH)+1;
-		
-		return Math.Abs(lH-rH)<2;
-	}
