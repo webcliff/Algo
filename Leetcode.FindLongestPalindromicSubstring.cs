981a64746aa1c9ffcb5ddebb8509da5713d08496
webcliff <webcliff@gmail.com>
2013-09-16 00:26:16 -0700

    Create Leetcode.FindLongestPalindromicSubstring.cs

---
+++ Leetcode.FindLongestPalindromicSubstring.cs
@@ -0,0 +1,25 @@
+	// Longest Palindromic Substring 
+	// http://leetcode.com/onlinejudge#question_5
+	
+	// Given a string S, find the longest palindromic substring in S. 
+	// You may assume that the maximum length of S is 1000, and there 
+	// exists one unique longest palindromic substring.
+	public string FindLongestPalindromicSubstring(string S) {
+		if(S==null) throw new ArgumentNullException("S");
+		if(S.Length<2) return S;
+
+		bool[,] f = new bool[S.Length,S.Length];
+		int maxLen=int.MinValue, start=int.MinValue;
+		for(int i=S.Length-1;i>=0;--i) {
+			for(int j=i;j<S.Length;++j) {
+				if(i==j) f[i,j]=true;
+				else if(j==i+1) f[i,j]=(S[i]==S[j]);
+				else f[i,j]=(f[i+1,j-1]&&S[i]==S[j]);
+
+				if(f[i,j]&&(j-i+1)>maxLen) {
+					maxLen=j-i+1; start=i;
+				}
+			}
+		}
+		return S.Substring(start,maxLen);
+	}
