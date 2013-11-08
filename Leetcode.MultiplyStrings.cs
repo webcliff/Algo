ce2545ad56591f3d9855f3be442437a62d45b36e
webcliff <webcliff@gmail.com>
2013-11-07 23:27:51 -0800

    Update Leetcode.MultiplyStrings.cs

--- Leetcode.MultiplyStrings.cs
+++ Leetcode.MultiplyStrings.cs
@@ -7,45 +7,72 @@
 		if(A==null||B==null) throw new ArgumentNullException();
 		if(A.Length==0||B.Length==0) throw new ArgumentException();
 
-		StringBuilder sb = new StringBuilder('0');
-		StringBuilder product = new StringBuilder();
+		//check if A and B are all digits
 
+		int numOfZeros=0;
+		string result="0";
 		for(int b=B.Length-1;b>=0;--b) {
-			MultiplyStringAndChar(product,A,B[b],B.Length-1-b);
-			Add(sb,product.ToString());
+			string product = MultipleStringwithChar(A,B[b],numOfZeros++);
+			result = Add(result,product);
 		}
+		return result;
+	}
+
+	private string MultipleStringwithChar(string A, char b, int numOfZeros) {
+		StringBuilder product = new StringBuilder();
+		while(numOfZeros>0) {
+			product.Append("0"); 
+			--numOfZeros;
+		}
+		int carry=0, ib=b-'0';
+		for(int a=A.Length-1;a>=0;--a) {
+			int ia=A[a]-'0';
+			ia=ia*ib+carry;
+			carry=ia/10; ia=ia%10;
+			product.Insert(0,ia);
+		}
+		if(carry>0) product.Insert(0,carry);
+		return product.ToString();
+	}
+
+	private string Add(string A, string B) {
+		StringBuilder sb = new StringBuilder();
+		int carry=0;
+		int a=A.Length-1,b=B.Length-1;
+		while(a>=0||b>=0) {
+			int ia=a>=0 ? A[a]-'0':0;
+			int ib=b>=0 ? B[b]-'0':0;
+			ia+=ib+carry;
+			carry=ia/10; ia=ia%10;
+			sb.Insert(0,ia);
+			--a;--b;
+		}
+		if(carry>0) sb.Insert(0,carry);
 		return sb.ToString();
 	}
+/*
+123,45
+b numOfZeros product result
+1 0          615     0,615
+4 1          4920    5535
 
-	private void MultiplyStringAndChar(StringBuilder product, string A, char b, int numZeros) {
-		product.Clear();
-		if(b-'0'==0) { product.Append('0'); return;}
 
-		while(numZeros>0) { product.Insert(0,'0'); --numZeros; }
 
-		int bi=b-'0', carry=0;
-		for(int a=A.Length-1;a>=0;--a) {
-			int ai=A[a]-'0';
-			ai=ai*bi+carry;
-			carry=ai/10; ai=ai%10;
-			product.Insert(0,(char)('0'+ai));
-		}
-		if(carry!=0) product.Insert(0,(char)('0'+carry));
-	}
+123,5,1 
+product 0
+carry ib a ia           product
+0,1   5  2 3,15,5       50
+1     5  1 2,11,1       150
+0     5  0 1,6,6        6150
 
-	private void Add(StringBuilder sb, string product) {
-		string sum=sb.ToString();
-		sb.Clear();
-		int s=sum.Length-1,p=product.Length-1;
-		int carry=0;
-		while(s>=0||p>=0) {
-			int si= (s>=0 ? sum[s]-'0' : 0);
-			int pi= (p>=0 ? product[p]-'0' : 0);
-			si =si+pi+carry;
-			carry=si/10; si=si%10;
-			sb.Insert(0,(char)(si+'0'));
-			--s;--p;
-		}
-		if(carry>0) sb.Insert(0,(char)(carry+'0'));
-	}
 
+
+
+123
+ 45
+a b ia ib carry  sb
+2 1 8  5  0      8
+1 0 6  4  0      6
+0-1 1  0  0      1
+return 168
+*/
